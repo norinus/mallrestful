55872a171eb94fa3048c44c9390ef53e0d345598
mankatos <klaatus@msn.com>
2024-08-23 08:58:23 +0900

    컨트롤러 에러 핸들러 추가

--- mall/src/main/java/com/klaatus/mall/controller/TodoController.java
+++ mall/src/main/java/com/klaatus/mall/controller/TodoController.java
@@ -11,7 +11,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -22,37 +21,37 @@ public class TodoController {
 
     private final TodoService todoService;
 
-
     @PostMapping("/")
-    public Map<String,Long> create( @RequestBody TodoDTO todoDTO){
-        
-        log.info("---------------------------> 값:{}", todoDTO);
-        
-        
+    public Map<String, Long> create(@RequestBody TodoDTO todoDTO) {
         return Map.of("TNO:", todoService.create(todoDTO));
     }
 
     @GetMapping("/{tno}")
-    public TodoDTO read(@PathVariable Long tno){
+    public TodoDTO read(@PathVariable Long tno) {
         return todoService.read(tno);
     }
 
     @PutMapping("/{tno}")
-    public Map<String,String> update(@PathVariable Long tno, @RequestBody TodoDTO todoDTO){
+    public Map<String, String> update(@PathVariable Long tno, @RequestBody TodoDTO todoDTO) {
         todoService.update(tno, todoDTO);
 
         return Map.of("RESULT", "SUCCESS");
     }
 
     @DeleteMapping("/{tno}")
-    public Map<String,String> delete(@PathVariable Long tno){
+    public Map<String, String> delete(@PathVariable Long tno) {
         todoService.delete(tno);
         return Map.of("RESULT", "SUCCESS");
     }
 
     @GetMapping("/list")
-    public Page<TodoDTO> list(@PageableDefault(size=100, sort="tno", direction = Sort.Direction.DESC) Pageable pageable){
-       return todoService.list(pageable);
+    public Page<TodoDTO> list(@PageableDefault(size = 10, sort = "tno", direction = Sort.Direction.DESC)Pageable pageable) {
+
+        log.info("Sort: {}", pageable.getSort());
+
+        log.info("페이지 정보: {}", pageable);
+
+        return todoService.list(pageable);
     }
 
 }
