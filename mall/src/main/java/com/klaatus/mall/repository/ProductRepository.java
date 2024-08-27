8d9a25c9ab3c693b15333b3f1aa45b65b6495b35
mankatos <klaatus@msn.com>
2024-08-27 17:56:56 +0900

    상품읽기 1. ProductRepository : 사용안함 주석 추가

--- mall/src/main/java/com/klaatus/mall/repository/ProductRepository.java
+++ mall/src/main/java/com/klaatus/mall/repository/ProductRepository.java
@@ -8,12 +8,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
-
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
 
     @EntityGraph(attributePaths = "imageList")
     Optional<Product> findByPno(Long pno);
